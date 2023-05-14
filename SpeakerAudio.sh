c18fb15a7f954e1782164b63b34e14d6d50e85f4
Carlos <34244849+carloslmar@users.noreply.github.com>
2023-05-14 14:08:58 -0500

    Restart should no longer be necessary

--- SpeakerAudio.sh
+++ SpeakerAudio.sh
@@ -4,4 +4,4 @@ source_file="/etc/adio/sp.conf"
 destination_file="/etc/asound.conf"
 # Copy the source file to the destination
 sudo cp "${source_file}" "${destination_file}"
-sudo reboot
+systemctl --user restart pulseaudio
