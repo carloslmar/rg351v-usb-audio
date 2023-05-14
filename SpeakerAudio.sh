75b4a302902a0cef9ec5d2593286fac111b473d6
Carlos <34244849+carloslmar@users.noreply.github.com>
2023-05-14 01:03:38 -0500

    Add files via upload

---
+++ SpeakerAudio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Define the source and destination file paths
+source_file="/etc/adio/sp.conf"
+destination_file="/etc/asound.conf"
+# Copy the source file to the destination
+sudo cp "${source_file}" "${destination_file}"
+sudo reboot
